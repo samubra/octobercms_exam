37ab036676c4df3ca544d7f4287262260fe17ed2
vagrant <vagrant@homestead>
2019-03-19 13:05:34 +0000

    添加用户组，模块，用户部分内容

--- models/User.php
+++ models/User.php
@@ -26,5 +26,19 @@ class User extends Model
      * @var array Validation rules
      */
     public $rules = [
+        'user_name'                  => 'required|between:4,16',
+        'user_email'                 => 'required|email',
+        'user_password'              => 'required|alpha_num|min:4|confirmed',
+        'user_password_confirmation' => 'required|alpha_num|min:4'
     ];
+
+    protected $fillable = ['user_name','user_password','user_email','user_regdate','user_ip','user_firstname','user_lastname','user_birthdate','user_birthplace','user_regnumber','user_ssn','user_level','user_verifycode','user_otpkey'];
+
+    public function beforeCreate()
+    {
+        $post = post();
+        $this->password = password_hash($post['User']['user_password'], PASSWORD_DEFAULT);
+        $this->user_regdate = now()->toDateTimeString();
+        $this->user_ip = request()->getClientIp();
+    }
 }
