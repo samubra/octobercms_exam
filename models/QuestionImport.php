5b877f2939b6fb77f85e41cb87fa227289f3fadc
samubra <samubrapeng@gmail.com>
2019-05-30 09:58:17 +0800

    验证导入题库（不重复导入)

--- models/QuestionImport.php
+++ models/QuestionImport.php
@@ -27,68 +27,68 @@ class QuestionImport extends \Backend\Models\ImportModel
         foreach ($results as $row => $data) {
 
             try {
-                $question = new Question();
-            	$question->fill($data);
-                $question_type = $question->question_type;
+                if (Question::where('question_tq_id', $data['question_tq_id'])->get()->count() === "0"){
+                    $question = new Question();
+                    $question->fill($data);
+                    $question_type = $question->question_type;
 
 
-                $question->question_difficulty = isset($data['question_difficulty'])?$data['question_difficulty']:'1';
-                $question->question_enabled = isset($data['question_enabled'])?$data['question_enabled']:1;
-                $question->question_auto_next = isset($data['question_auto_next'])?$data['question_auto_next']:1;
+                    $question->question_difficulty = isset($data['question_difficulty']) ? $data['question_difficulty'] : '1';
+                    $question->question_enabled = isset($data['question_enabled']) ? $data['question_enabled'] : 1;
+                    $question->question_auto_next = isset($data['question_auto_next']) ? $data['question_auto_next'] : 1;
 
-                if($question->question_type === '5'){
-                    $question->question_type = '1';
+                    if ($question->question_type === '5') {
+                        $question->question_type = '1';
+                    }
+                    $question->save();
+                    trace_log($question->question_id);
+
+                    $answerList = [];
+                    if ($question_type === '5') {
+                        $answerList = [
+                            new Answer([
+                                'answer_description' => '对',
+                                'answer_isright' => $data['answer'] === '对',
+                                'answer_enabled' => '1',
+                                'answer_question_id' => $question->question_id
+                            ]),
+                            new Answer([
+                                'answer_description' => '错',
+                                'answer_isright' => $data['answer'] === '错',
+                                'answer_enabled' => '1',
+                                'answer_question_id' => $question->question_id
+                            ]),
+                        ];
+                    } else {
+                        $answerList = [
+                            new Answer([
+                                'answer_description' => $data['answer_one'],
+                                'answer_isright' => $data['answer'] === 'A',
+                                'answer_enabled' => '1',
+                                'answer_question_id' => $question->question_id
+                            ]),
+                            new Answer([
+                                'answer_description' => $data['answer_two'],
+                                'answer_isright' => $data['answer'] === 'B',
+                                'answer_enabled' => '1',
+                                'answer_question_id' => $question->question_id
+                            ]),
+                            new Answer([
+                                'answer_description' => $data['answer_three'],
+                                'answer_isright' => $data['answer'] === 'C',
+                                'answer_enabled' => '1',
+                                'answer_question_id' => $question->question_id
+                            ]),
+                        ];
+                    }
+
+                    //$question->answers()->addMany($answerList);
+
+                    foreach ($answerList as $answer) {
+                        $answer->save();
+                    }
+                    $this->logCreated();
                 }
-                $question->save();
-                trace_log($question->question_id);
-
-                $answerList = [];
-                if($question_type === '5'){
-                    $answerList = [
-                        new Answer([
-                            'answer_description'=>'对',
-                            'answer_isright'=> $data['answer'] === '对',
-                            'answer_enabled' => '1',
-                            'answer_question_id' => $question->question_id
-                        ]),
-                        new Answer([
-                            'answer_description'=>'错',
-                            'answer_isright'=> $data['answer'] === '错',
-                            'answer_enabled' => '1',
-                            'answer_question_id' => $question->question_id
-                        ]),
-                    ];
-                }else{
-                    $answerList = [
-                        new Answer([
-                            'answer_description'=>$data['answer_one'],
-                            'answer_isright'=> $data['answer'] === 'A',
-                            'answer_enabled' => '1',
-                            'answer_question_id' => $question->question_id
-                        ]),
-                        new Answer([
-                            'answer_description'=>$data['answer_two'],
-                            'answer_isright'=> $data['answer'] === 'B',
-                            'answer_enabled' => '1',
-                            'answer_question_id' => $question->question_id
-                        ]),
-                        new Answer([
-                            'answer_description'=>$data['answer_three'],
-                            'answer_isright'=> $data['answer'] === 'C',
-                            'answer_enabled' => '1',
-                            'answer_question_id' => $question->question_id
-                        ]),
-                    ];
-                }
-
-                //$question->answers()->addMany($answerList);
-
-                foreach ($answerList as $answer){
-                    $answer->save();
-                }
-
-
-                $this->logCreated();
             }
             catch (\Exception $ex) {
                 $this->logError($row, $ex->getMessage());
